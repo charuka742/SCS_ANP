10123fd812182ca6d49039e9bb87fca0c75e3df1
Disanayaka AAC <charair742@gmail.com>
2023-09-26 00:59:16 +0530

    Initialized Project

---
+++ server/src/main/java/com/PeraAlumniSci/PeraAlumniSci/PeraAlumniSciApplication.java
@@ -0,0 +1,13 @@
+package com.PeraAlumniSci.PeraAlumniSci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeraAlumniSciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeraAlumniSciApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/PeraAlumniSci/PeraAlumniSci/PeraAlumniSciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PeraAlumniSci.PeraAlumniSci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeraAlumniSciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
